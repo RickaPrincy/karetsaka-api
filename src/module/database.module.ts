625e699d4f5f90424e8ba0b9db60332f52ae4f6a
RickaPrincy <rckprincy@gmail.com>
2024-05-26 06:16:56 +0000

    infra: rename env

--- src/module/database.module.ts
+++ src/module/database.module.ts
@@ -1,4 +1,4 @@
-import {ConfigService} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 import {config} from "dotenv";
 import {Module} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
@@ -8,20 +8,17 @@ import {Image} from "src/model/image.entity";
 import {User} from "src/model/user.entity";
 import {Dummy} from "src/model/dummy.entity";
 
-config();
-const configService = new ConfigService();
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: configService.get("POSTGRES_HOSTNAME"),
-      port: Number(configService.get("POSTGRES_PORT")),
-      username: configService.get("POSTGRES_USER"),
-      password: configService.get("POSTGRES_PASSWORD"),
-      database: configService.get("POSTGRES_DATABASE"),
-      entities: [Dummy, User, Car, Image, Appointment],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        url: configService.get("DATABASE_URL"),
+        entities: [Dummy, User, Car, Image, Appointment],
+        synchronize: true,
+      }),
     }),
   ],
 })
