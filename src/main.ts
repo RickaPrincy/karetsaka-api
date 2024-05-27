b74e993f9f588e20fe04bce96a34b4fbbb664cfd
RickaPrincy <rckprincy@gmail.com>
2024-05-27 23:04:46 +0300

    chore: setup swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,34 @@
 import {NestFactory} from "@nestjs/core";
 import {KaretsakaModule} from "./karetsaka.module";
 import {config} from "dotenv";
-import {ValidationPipe} from "@nestjs/common";
+import {INestApplication, ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 config();
-async function bootstrap() {
-  const app = await NestFactory.create(KaretsakaModule);
-  app.useGlobalPipes(new ValidationPipe());
 
+function setupSwagger<T>(app: INestApplication<T>) {
+  const openapiConfig = new DocumentBuilder()
+    .setTitle("Karetsaka Api")
+    .setDescription("Karetsaka")
+    .setVersion("0.0.1")
+    .addServer("https://karetsaka-api.adaptable.app")
+    .addTag("Health")
+    .addTag("Security")
+    .addTag("Cars")
+    .addSecurity("Firebase Authentification", {
+      type: "http",
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, openapiConfig);
+  SwaggerModule.setup("docs", app, document);
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(KaretsakaModule, {
+    logger: ["error", "warn", "log", "verbose", "fatal"],
+  });
+  setupSwagger(app);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(+(process.env.PORT || 3000));
 }
--- src/model/dummy.entity.ts
+++ src/model/dummy.entity.ts
@@ -1,10 +1,13 @@
+import {ApiProperty} from "@nestjs/swagger";
 import {Column, Entity, PrimaryColumn} from "typeorm";
 
 @Entity()
 export class Dummy {
+  @ApiProperty({format: "uuid", required: true})
   @PrimaryColumn()
   id: string;
 
+  @ApiProperty({required: true})
   @Column()
   name: string;
 }
