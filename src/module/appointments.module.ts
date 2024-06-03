6ad81b2632ddbe4219a25d7223d90dde3d517c91
Daris <103503966+Daris02@users.noreply.github.com>
2024-06-04 01:27:27 +0300

    Feat/appointment
    * fix: add api decorator
    
    * fix: add api decorator
    
    * fix: add firebase module

--- src/module/appointments.module.ts
+++ src/module/appointments.module.ts
@@ -3,9 +3,10 @@ import {AppointmentsController} from "../controller/appointments.controller";
 import {AppointmentsService} from "../service/appointments.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Appointment} from "src/model/appointment.entity";
+import {FirebaseModule} from "./firebase.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Appointment])],
+  imports: [TypeOrmModule.forFeature([Appointment]), FirebaseModule],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
 })
