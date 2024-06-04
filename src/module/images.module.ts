fe9c093e392fe87d555fae4afbeef725bfb0446b
Daris02 <hei.raymond.2@gmail.com>
2024-06-05 00:22:44 +0300

    chore: add import firebase module

--- src/module/images.module.ts
+++ src/module/images.module.ts
@@ -3,9 +3,10 @@ import {ImagesController} from "../controller/images.controller";
 import {ImagesService} from "../service/images.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Image} from "src/model/image.entity";
+import { FirebaseModule } from "./firebase.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
+  imports: [TypeOrmModule.forFeature([Image]), FirebaseModule],
   controllers: [ImagesController],
   providers: [ImagesService],
 })
